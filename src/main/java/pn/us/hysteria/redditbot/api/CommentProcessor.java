16f93c0a194456d7fffd838bde01a3e903b4b2c5
KingBowser <backseat.rapist@gmail.com>
2013-06-30 14:06:06 -0500

    Added interfaces for comment/post/message processing

---
+++ src/main/java/pn/us/hysteria/redditbot/api/CommentProcessor.java
@@ -0,0 +1,12 @@
+package pn.us.hysteria.redditbot.api;
+
+/**
+ * Processes comments discovered by the bot
+ * Creator: KingBowser
+ * Date: 6/30/13
+ * Time: 9:43 AM
+ * Refer to LICENSE for licensing information
+ */
+public interface CommentProcessor {
+
+}
---
+++ src/main/java/pn/us/hysteria/redditbot/api/MessageProcessor.java
@@ -0,0 +1,12 @@
+package pn.us.hysteria.redditbot.api;
+
+/**
+ * Processes messages received by the bot
+ * Creator: KingBowser
+ * Date: 6/30/13
+ * Time: 9:43 AM
+ * Refer to LICENSE for licensing information
+ */
+public interface MessageProcessor {
+
+}
